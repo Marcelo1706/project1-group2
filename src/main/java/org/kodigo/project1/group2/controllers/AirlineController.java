5d7d9267505d27475a07e6fe25a6fb9173736cfc
nestorrecinosUCA <62679356+nestorrecinosUCA@users.noreply.github.com>
2021-08-19 22:18:22 -0600

    :sparkles: AirlineController updated

--- src/main/java/org/kodigo/project1/group2/controllers/AirlineController.java
+++ src/main/java/org/kodigo/project1/group2/controllers/AirlineController.java
@@ -5,10 +5,32 @@
  */
 package org.kodigo.project1.group2.controllers;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.kodigo.project1.group2.models.Airline;
+import org.kodigo.project1.group2.models.Database;
+
 /**
  *
  * @author Néstor Recinos
  */
 public class AirlineController {
+    @Getter @Setter private Airline singleAirline;
+    @Getter @Setter private Database databaseHandler;
     
+    public AirlineController(){
+        this.databaseHandler = new Database();
+    }
+    
+   public boolean newAirline(String airlineName){
+       return databaseHandler.insert("airline", "airlineName", "'"+airlineName+"'");
+   }
+   
+   public boolean updateAirline(int id, String airlineName){
+       return databaseHandler.update("airline", airlineName, airlineName);
+   }
+           
+   public boolean deleteAirline(int id){
+       return databaseHandler.delete("airline", "airlineId = " + id);
+   }
 }
