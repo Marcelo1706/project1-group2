3b7bb3f565faf84a78b2d14a616cac7c3668fa81
nestorrecinosUCA <62679356+nestorrecinosUCA@users.noreply.github.com>
2021-08-19 19:40:06 -0600

    Update AircraftController.java

--- src/main/java/org/kodigo/project1/group2/controllers/AircraftController.java
+++ src/main/java/org/kodigo/project1/group2/controllers/AircraftController.java
@@ -5,10 +5,33 @@
  */
 package org.kodigo.project1.group2.controllers;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.kodigo.project1.group2.models.Aircraft;
+import org.kodigo.project1.group2.models.Airline;
+import org.kodigo.project1.group2.models.Database;
+
 /**
  *
  * @author Néstor Recinos
  */
 public class AircraftController {
+    @Getter @Setter private Aircraft singleCountry;
+    @Getter @Setter private Database databaseHandler;
     
+    public AircraftController(){
+        this.databaseHandler = new Database();
+    }
+    
+    public boolean newAircraft(String model, int passengerCapacity, double fuelRange, Airline airline){
+        return databaseHandler.insert("aircraft", "model, passengerCapacity, fuelRange, airline", "'"+model+"','"+passengerCapacity+"','"+fuelRange+"','"+airline.getAirlineId()+"'");
+    }
+    
+    public boolean updateAircraft(int id, String model, int passengerCapacity, double fuelRange, Airline airline){
+        return databaseHandler.update("aircraft", "model = '"+model+"', passengerCapacity = '"+passengerCapacity+"', fuelRange = '"+fuelRange+"', airline = '"+airline.getAirlineId()+"'", "aircraftId = " + id);
+    }
+    
+    public boolean deleteAircraft(int id){
+        return databaseHandler.delete("aircraft", "aircraftId = " + id);
+    }
 }
